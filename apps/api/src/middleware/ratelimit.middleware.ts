efa0609d77cf2f318ff71ba8593f3c202e7b04c5
akdevsaha-dev <akdevsaha.dev@gmail.com>
2026-05-02 21:30:43 +0530

    feat: add rate limit to signin and signup routes

--- apps/api/src/middleware/ratelimit.middleware.ts
+++ apps/api/src/middleware/ratelimit.middleware.ts
@@ -1,9 +1,47 @@
+import { redis } from "@repo/lib";
 import type { Request, Response, NextFunction } from "express";
 
-export const authRateLimit = (
+const CAPACITY = 10;
+const REFILL_RATE = 1;
+const EXPIRE_SECONDS = 60;
+
+export const authRateLimit = async (
   req: Request,
   res: Response,
   next: NextFunction,
 ) => {
-    
+  try {
+    const identifier = req.user?.id ?? req.ip;
+    const key = `bucker:${identifier}`;
+    const now = Date.now();
+    const data = await redis.hGetAll(key);
+
+    let tokens = data.tokens ? parseFloat(data.tokens) : CAPACITY;
+    let lastRefill = data.lastRefil ? parseFloat(data.lastRefil) : now;
+
+    const ellapsedSeconds = (now - lastRefill) / 1000;
+    const refill = ellapsedSeconds * REFILL_RATE;
+
+    tokens = Math.min(CAPACITY, tokens + refill);
+
+    if (tokens < 1) {
+      return res.status(429).json({
+        success: false,
+        message: "Rate limit exceeded. Try again later.",
+      });
+    }
+    tokens -= 1;
+
+    await redis.hSet(key, {
+      tokens: tokens.toString(),
+      lastRefill: now.toString(),
+    });
+
+    await redis.expire(key, EXPIRE_SECONDS);
+
+    next();
+  } catch (error) {
+    console.error("Token bucket error:", error);
+    next();
+  }
 };
--- apps/api/src/routes/auth.route.ts
+++ apps/api/src/routes/auth.route.ts
@@ -1,9 +1,10 @@
 import express, { Router } from "express";
 import { signin, signout, signup } from "../handlers/auth.handler";
+import { authRateLimit } from "../middleware/ratelimit.middleware";
 
 const router = Router();
 
-router.post("/signup", signup);
-router.post("/signin", signin);
+router.post("/signup", authRateLimit, signup);
+router.post("/signin", authRateLimit, signin);
 router.post("/signout", signout);
 export default router;
